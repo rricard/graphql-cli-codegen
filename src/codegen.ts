7a7bba9fc5a2a338f2f87243e82c90551e6132b0
Robin Ricard <ricard.robin@gmail.com>
2017-08-07 17:00:39 +0200

    Require semicolons with tslint

--- src/codegen.ts
+++ src/codegen.ts
@@ -1,60 +1,60 @@
 import {
   generate,
-} from 'apollo-codegen'
+} from 'apollo-codegen';
 import {
   buildASTSchema,
   parse,
   GraphQLSchema,
   graphql,
-} from 'graphql'
-import { introspectionQuery } from 'graphql/utilities'
-import * as path from 'path'
-import * as fs from 'fs'
-import * as glob from 'glob'
+} from 'graphql';
+import { introspectionQuery } from 'graphql/utilities';
+import * as path from 'path';
+import * as fs from 'fs';
+import * as glob from 'glob';
 
-export const command = 'codegen [--target] [--output]'
-export const desc = 'Generates apollo-codegen code/annotations from your .graphqlconfig'
-export const builder = {}
+export const command = 'codegen [--target] [--output]';
+export const desc = 'Generates apollo-codegen code/annotations from your .graphqlconfig';
+export const builder = {};
 
 const globAsync = (globPath: string): Promise<string[]> => {
   return new Promise((res, rej) => {
     glob(globPath, (err, paths) => {
       if (err) {
-        return rej(err)
+        return rej(err);
       }
-      res(paths || [])
-    })
-  })
-}
+      res(paths || []);
+    });
+  });
+};
 
 const getSchema = async (config, configPath): Promise<GraphQLSchema> => {
-  const configBase = path.dirname(configPath)
-  const schemaPath = path.join(configBase, config.schemaPath)
-  const schemaContents = fs.readFileSync(schemaPath).toString()
-  const schema = await buildASTSchema(parse(schemaContents))
-  return schema
-}
+  const configBase = path.dirname(configPath);
+  const schemaPath = path.join(configBase, config.schemaPath);
+  const schemaContents = fs.readFileSync(schemaPath).toString();
+  const schema = await buildASTSchema(parse(schemaContents));
+  return schema;
+};
 
 const writeSchema = async (schema: GraphQLSchema, path: string): Promise<void> => {
-  const results = await graphql(schema, introspectionQuery)
-  fs.writeFileSync(path, JSON.stringify(results))
-}
+  const results = await graphql(schema, introspectionQuery);
+  fs.writeFileSync(path, JSON.stringify(results));
+};
 
 const getInputs = async (config, configPath): Promise<string[]> => {
-  const configBase = path.dirname(configPath)
-  const includes: string[] = config.includes || []
-  const globResults = await Promise.all(includes.map(g => globAsync(g)))
-  return globResults.reduce((acc, r) => [...acc, ...r])
-}
+  const configBase = path.dirname(configPath);
+  const includes: string[] = config.includes || [];
+  const globResults = await Promise.all(includes.map(g => globAsync(g)));
+  return globResults.reduce((acc, r) => [...acc, ...r]);
+};
 
 export const handler = async ({getConfig}) => {
-  const {config, configPath} = getConfig()
-  const options = (config.extensions || {}).codegen || {}
-  const configBase = path.dirname(configPath)
-  const schema = await getSchema(config, configPath)
-  const schemaLoc = '/tmp/gql-schema.json' // todo: change this
-  await writeSchema(schema, schemaLoc)
-  const inputFiles = await getInputs(config, configPath)
+  const {config, configPath} = getConfig();
+  const options = (config.extensions || {}).codegen || {};
+  const configBase = path.dirname(configPath);
+  const schema = await getSchema(config, configPath);
+  const schemaLoc = '/tmp/gql-schema.json'; // todo: change this
+  await writeSchema(schema, schemaLoc);
+  const inputFiles = await getInputs(config, configPath);
   generate(
     inputFiles,
     schemaLoc,
@@ -62,5 +62,5 @@ export const handler = async ({getConfig}) => {
     options.target || 'swift' ,
     options.tagName || 'gql',
     options,
-  )
-}
+  );
+};
